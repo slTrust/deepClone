cef24cefeda12307504aa69e594373334554e797
huangjiaxi <huangjiaxi@oak-inc.com>
2020-03-04 09:26:01 +0800

    遗留一个爆栈问题

--- test/index.js
+++ test/index.js
@@ -81,6 +81,23 @@ describe('deepClone',()=>{
 
         })
 
+        xit("不会爆栈",()=>{
+            const a = {child:null};
+            let b = a
+            for(let i=0;i<20000;i++){
+                b.child = {
+                    child:null
+                }
+                b = b.child
+            }
+            const a2 = deepClone(a);
+            assert(a !== a2);
+            assert(a.child !== a2.child);
+
+        })
+
+
+
     })
 
     
