b4fc0a8cd31f1966a1718301ce30f1231677a50d
huangjiaxi <huangjiaxi@oak-inc.com>
2020-03-04 08:57:11 +0800

    基本雏形完成

--- test/index.js
+++ test/index.js
@@ -37,5 +37,40 @@ describe('deepClone',()=>{
         
     })
 
+    describe("对象",()=>{
+        it("能够复制普通对象",()=>{
+            const a = {name: "方方", chlid: {name: "小方方"}};
+            const a2 = deepClone(a);
+            assert(a !== a2);
+            assert(a.name === a2.name);
+            assert(a.chai !== a2.chlid);
+            assert(a.chlid.name === a2.chlid.name);
+        })
+
+        it("能够复制数组对象",()=>{
+            const a = [[11,12],[21,22],[31,32]]
+            const a2 = deepClone(a);
+            assert(a !== a2);
+            assert(a[0] !== a2[0]);
+            assert(a[1] !== a2[1]);
+            assert(a[2] !== a2[2]);
+            assert.deepEqual(a,a2);
+        })
+
+        it("能够复制函数",()=>{
+            const a = function(x,y){
+                return x + y;
+            }
+            a.xxx = { yyy: { zzz: 1 } }
+            const a2 = deepClone(a);
+            assert(a !== a2);
+            assert(a.xxx.yyy.zzz === a2.xxx.yyy.zzz);
+            assert(a.xxx.yyy !== a2.xxx.yyy);
+            assert(a.xxx !== a2.xxx);
+            assert(a(1,2) === a2(1,2));
+
+        })
+    })
+
     
 })
